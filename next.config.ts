6bb83eb89d5a67ceb7f0f850e29922e82287707f
Bao Huynh <124868331+HyTiBa@users.noreply.github.com>
2025-02-15 07:38:12 +0700

    new configugation

--- next.config.ts
+++ next.config.ts
@@ -2,33 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // output: "export",
+  webpack:(config,{isServer})=>{
+    if (!isServer) {
+      config.resolve = {
+          ...config.resolve,
+          fallback: {
+              fs: false,
+          },
+      };
+  }
+  return config;
+  },
   eslint:{
     ignoreDuringBuilds:true
   }
 };
 
-module.exports = {
-
-  // Can be safely removed in newer versions of Next.js
-  future: {
-
-    // by default, if you customize webpack config, they switch back to version 4.
-    // Looks like backward compatibility approach.
-    webpack5: true,   
-  },
-
-  webpack(config: { resolve: { fallback: any; }; }) {
-    config.resolve.fallback = {
-
-      // if you miss it, all the other options in fallback, specified
-      // by next.js will be dropped.
-      ...config.resolve.fallback,  
-
-      fs: false, // the solution
-    };
-    
-    return config;
-  },
-};
 
 export default nextConfig;
